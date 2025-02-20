37d95c15a73060eea38faf9aea47d91d5784dde2
Varya Kush <barbarakushaeva@gmail.com>
2025-02-20 18:05:41 +0300

    Сделан дизайн сайта

--- server.js
+++ server.js
@@ -4,20 +4,16 @@ const path = require("path");
 const app = express();
 const PORT = 3000;
 
-// Указываем папку с статическими файлами
 app.use(express.static(path.join(__dirname, "public")));
 
-// Основная страница
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
-// Обработка всех остальных маршрутов (404)
 app.use((req, res) => {
     res.status(404).sendFile(path.join(__dirname, "public", "error.html"));
 });
 
-// Запуск сервера
 app.listen(PORT, () => {
     console.log(`Сервер запущен на http://localhost:${PORT}`);
 });
